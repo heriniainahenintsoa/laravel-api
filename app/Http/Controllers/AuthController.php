2abd3163e87a46eb6f6926fc0141fb01dd1c1bd7
heriniainahenintsoa <heriniaina@poem-group.com>
2025-05-15 17:00:38 +0300

    Done

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            "name" => "required|max:255",
+            "email" => "unique:users|email|required",
+            "password" => "required|min:8|confirmed"
+        ]);
+
+        $user = User::create($fields);
+
+        $token = $user->createToken($user->name);
+
+        return ["user" => $user, "token" => $token->plainTextToken];
+    }
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            "email" => "exists:users|email|required",
+            "password" => "required|min:8"
+        ]);
+        $user = User::where('email', $fields['email'])->first();
+
+        if (!$user || !Hash::check($fields['password'], $user->password)) {
+            return ['message' => "Bad credentials"];
+        }
+
+        $token = $user->createToken($user->name);
+
+        return ["user" => $user, "token" => $token->plainTextToken];
+    }
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return ['message' => "Logged out successfully"];
+    }
+}
